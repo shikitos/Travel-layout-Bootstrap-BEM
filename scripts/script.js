269f2bfdbf2ba7a89ca87dce74ebf48c113578f0
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-05-03 00:02:32 +0500

    Finish DISCOUNT and BIGDEAL sections
    Add one modal in the DISCOUNT SECTION, edit carousel and add new imgs. Finish BIGDEAL section.

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,26 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal2");
 // Get the button that opens the modal
-var btn = document.getElementById("openModal");
-// Get the <span> element that closes the modal
+var btn = document.getElementById("openModal2");
+var btn2 = document.getElementById("openModal2")
+    // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    //Open modal at the button from DISCOUNT section
+btn2.onclick = function() {
         modal.style.display = "block";
     }
     // When the user clicks on <span> (x), close the modal
@@ -50,3 +65,48 @@ window.onload = function() {
 function sendButton() {
     alert("Thank you for your respond!");
 }
+
+
+//Сountdown
+
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 365 * 24 * 60 * 60 * 1000); // for endless timer
+initializeClock('countdown', deadline);
