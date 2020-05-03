5058c49446b7db5836f69e0b0e9a962d5f117aac
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-05-04 00:09:29 +0500

    Correct modal2 in the getticket SECTION
    Try to correct modal window (position of the modal window and inner position of the elements in the modal)

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,11 @@
 // Get the modal
-var modal = document.getElementById("myModal2");
+var modal = document.getElementById("myModal");
 // Get the button that opens the modal
-var btn = document.getElementById("openModal2");
-var btn2 = document.getElementById("openModal2")
-    // Get the <span> element that closes the modal
+var btn = document.getElementById("openModal");
+// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-        modal.style.display = "block";
-    }
-    // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-        modal.style.display = "none";
-    }
-    // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-    //Open modal at the button from DISCOUNT section
-btn2.onclick = function() {
         modal.style.display = "block";
     }
     // When the user clicks on <span> (x), close the modal
@@ -33,6 +18,27 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+//Open modal at the button from DISCOUNT section
+var modal2 = document.getElementById("myModal2")
+var btn2 = document.getElementById("openModal2")
+var span2 = document.getElementsByClassName("close-2")[0];
+
+btn2.onclick = function() {
+        modal2.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+        modal2.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
+
 
 //Search section. Tabs
 function openType(evt, typeName) {
