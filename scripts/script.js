25da6810a43c977aa5073ac8ead350a977d8344a
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-05-05 23:30:22 +0500

    Finish the styling and the skeleton
    Add script for smooth scroll to the anchor, finish styling of the modal2, finish styling of the footer

--- scripts/script.js
+++ scripts/script.js
@@ -117,3 +117,20 @@ window.onclick = function(event) {
         modal2.style.display = "none";
     }
 }
+
+
+//Smooth scroll
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href').substr(1)
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
